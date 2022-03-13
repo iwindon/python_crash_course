6ff0ec1e5a979f56cb02103da7d0058c3288e118
Ivan Windon <ivan.windon@icloud.com>
2022-03-13 18:23:26 -0400

    Initial Init

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+# prints Hello Python World
+print("Hello Python World!")
+
+
+first_name = "Ivan"
+last_name = "Windon"
+#format's and combines first and last name
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+favorite_langauge = 'python ' # added white space 
+# prints with white space at the end
+print(favorite_langauge)
+# prints without white space
+print(favorite_langauge.rstrip())
+
+# removes the white space for good
+favorite_langauge = favorite_langauge.rstrip()
