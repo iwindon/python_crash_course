88278f2dd46e1ce6eb96624d6ea64c61a76ae624
Ivan Windon <ivan.windon@icloud.com>
2022-03-13 21:02:15 -0400

    Added challenges

---
+++ name_cases.py
@@ -0,0 +1,4 @@
+name = "Ivan windon"
+print(name.lower())
+print(name.upper())
+print(name.title())
---
+++ personal_messages.py
@@ -0,0 +1,2 @@
+name = "Ivan"
+print(f"Hello {name}, would you like to learn some Python today?")
